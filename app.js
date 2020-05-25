1df3add931a28537487e37281b3ce1aae4333538
José Tomaz <rbj.ribeiro@hotmail.com>
2020-05-25 01:19:50 -0300

    My deploy to Node.JS on Umbler

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server has started successfully");
+});
